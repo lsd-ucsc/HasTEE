32408619900c74b1d560a50e71968de91bfd2e96
martinovjulian <49378017+martinovjulian@users.noreply.github.com>
2024-06-20 17:39:37 +0000

    reverting to old example w/ enclave

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,8 @@ module Main where
 
 import Control.Monad.IO.Class(liftIO)
 
+import DynFlags
+
 import App
 import DCLabel
 #ifdef ENCLAVE
@@ -13,16 +15,38 @@ import Enclave
 import Client
 #endif
 
-import GHC
-import GHC.Paths ( libdir )
-import DynFlags ( defaultFatalMessager, defaultFlushOut )
 
-main :: IO SuccessFlag
-main =
-    defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
-      runGhc (Just libdir) $ do
-        dflags <- getSessionDynFlags
-        _ <- setSessionDynFlags dflags
-        target <- guessTarget "test_main.hs" Nothing
-        setTargets [target]
-        load LoadAllTargets
+data API =
+  API { checkpwd :: Secure (String -> EnclaveDC Bool) }
+
+
+pwdLabel :: DCLabel
+pwdLabel = "Alice" %% "Alice"
+
+pwdChecker :: EnclaveDC (DCLabeled String) -> String -> EnclaveDC Bool
+pwdChecker pwd guess = do
+  l_pwd <- pwd
+  priv  <- getPrivilege
+  p     <- unlabelP priv l_pwd
+  if p == guess
+  then return True
+  else return False
+
+client :: API -> Client "client" ()
+client api = do
+  liftIO $ putStrLn "Enter your password:"
+  userInput <- liftIO getLine
+  res <- gatewayRA ((checkpwd api) <@> userInput)
+  liftIO $ putStrLn ("Login returned " ++ show res)
+
+ifctest :: App Done
+ifctest = do
+  pwd   <- inEnclaveLabeledConstant pwdLabel "password"
+  let priv = toCNF "Alice"
+  efunc <- inEnclave (dcDefaultState priv) $ pwdChecker pwd
+  runClient (client (API efunc))
+
+main :: IO ()
+main = do
+  res <- runAppRA "client" ifctest
+  return $ res `seq` ()
