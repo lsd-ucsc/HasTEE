90d738bf1b1343e2953ce9d527d827adde0f965a
martinovjulian <49378017+martinovjulian@users.noreply.github.com>
2024-06-20 02:48:28 +0000

    work in prog

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,8 @@ module Main where
 import Control.Monad.IO.Class(liftIO)
 
 import GHC
-import GHC.Paths
-import DynFlags
+import GHC.Paths (libdir)
+import DynFlags (defaultLogAction)
 
 import App
 import DCLabel
@@ -18,37 +18,11 @@ import Client
 #endif
 
 
-data API =
-  API { checkpwd :: Secure (String -> EnclaveDC Bool) }
-
-
-pwdLabel :: DCLabel
-pwdLabel = "Alice" %% "Alice"
-
-pwdChecker :: EnclaveDC (DCLabeled String) -> String -> EnclaveDC Bool
-pwdChecker pwd guess = do
-  l_pwd <- pwd
-  priv  <- getPrivilege
-  p     <- unlabelP priv l_pwd
-  if p == guess
-  then return True
-  else return False
-
-client :: API -> Client "client" ()
-client api = do
-  liftIO $ putStrLn "Enter your password:"
-  userInput <- liftIO getLine
-  res <- gatewayRA ((checkpwd api) <@> userInput)
-  liftIO $ putStrLn ("Login returned " ++ show res)
-
-ifctest :: App Done
-ifctest = do
-  pwd   <- inEnclaveLabeledConstant pwdLabel "password"
-  let priv = toCNF "Alice"
-  efunc <- inEnclave (dcDefaultState priv) $ pwdChecker pwd
-  runClient (client (API efunc))
-
-main :: IO ()
-main = do
-  res <- runAppRA "client" ifctest
-  return $ res `seq` ()
+main =
+  defaultErrorHandler defaultLogAction $ do
+    runGhc (Just libdir) $ do
+      dflags <- getSessionDynFlags
+      setSessionDynFlags dflags
+      target <- guessTarget "test_main.hs" Nothing
+      setTargets [target]
+      load LoadAllTargets
